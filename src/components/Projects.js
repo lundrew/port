cd601425e82535b284894f405735080147b2fedf
lundrew <andrueee@gmail.com>
2022-06-08 12:31:50 -0700

    project div added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,14 +1,33 @@
 import project1 from './Images/Projects/calc.png';
 import project2 from './Images/Projects/rgb.png';
 
+import { IconContext } from 'react-icons';
+import { AiFillHtml5 } from 'react-icons/ai';
+import { IoLogoCss3 } from 'react-icons/io';
+import { IoLogoJavascript } from 'react-icons/io';
+import { FaReact } from 'react-icons/fa';
+import { FaGitAlt } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
+import { SiRubyonrails } from 'react-icons/si'
+import { DiRuby } from 'react-icons/di'
+import { SiPostman } from 'react-icons/si'
+import { SiPostgresql } from 'react-icons/si'
+
 const Projects = () => {
   return (
     <section id="projects" className="project-section">
       <h1 className="project-section-h1">Projects</h1>
+
+
+      <div className="scroll">
       <div className="projects-cnt">
         <div className="project-wrapper">
           <div className="project-description">
             <h1>Calculator</h1>
+            <FaReact className="project-icons" style={{ color: '#066BF7 ',fontSize: '2rem', }} />
+            <IoLogoJavascript className="project-icons" style={{color: '#000',backgroundColor: '#EDD62E ',fontSize: '2rem',}}/>
+            <AiFillHtml5 className="project-icons" style={{ color: '#f55a42',fontSize: '2rem', }} />
+            <IoLogoCss3 className="project-icons" style={{ color: '#066BF7',fontSize: '2rem', }} />
             <p></p>
             <div className="btn-wrapper">
               <a
@@ -34,6 +53,10 @@ const Projects = () => {
         <div className="project-wrapper row-reverse">
           <div className="project-description">
             <h1>RGB Generator</h1>
+            <FaReact className="project-icons" style={{ color: '#066BF7 ',fontSize: '2rem', }} />
+            <IoLogoJavascript className="project-icons" style={{color: '#000',backgroundColor: '#EDD62E ',fontSize: '2rem',}}/>
+            <AiFillHtml5 className="project-icons" style={{ color: '#f55a42',fontSize: '2rem', }} />
+            <IoLogoCss3 className="project-icons" style={{ color: '#066BF7',fontSize: '2rem', }} />
             <p></p>
             <div className="btn-wrapper">
               <a
@@ -60,7 +83,7 @@ const Projects = () => {
             />
           </div>
         </div>
-        
+        </div>
       </div>
     </section>
   );
